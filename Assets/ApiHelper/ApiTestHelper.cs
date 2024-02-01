c00e5e2b4fa14352c597b6ce97c36e1e5dbe8f48
AzureAD\ALEIXRAMIREZIPEREZ <aleix.ramirez@enti.cat>
2024-02-01 17:30:05 +0100

    ui

--- Assets/ApiHelper/ApiTestHelper.cs
+++ Assets/ApiHelper/ApiTestHelper.cs
@@ -7,105 +7,100 @@ using UnityEngine.UI;
 
 public class ApiTestHelper : MonoBehaviour
 {
-    [Header("Api call Setup")]
-    public string url = "";
-    public Dictionary<string, string> parameters = new();
+        [Header("Api call Steup")]
+        public string url = "";
+        public Dictionary<string, string> parameters = new();
 
-    [Header("Objects")]
-    public TextMeshProUGUI resultField;
-    public RawImage imageField;
+        [Header("Objects")]
+        public TextMeshProUGUI resultField;
+        public RawImage imageField;
 
-  public void MakeApiCall()
-    {
-        IEnumerator apiCall = ApiHelper.Get(url, parameters, OnSuccess, OnFailure);
-
-        resultField.text = "In Progress";
-
-        StartCoroutine(apiCall);
-    }
-
-    private void OnFailure(Exception exception)
-    {
-        resultField.text = "Call Error:" + "<br>" + exception.Message;
-    }
-
-    private void OnSuccess(string result) 
-    {
-        resultField.text = result;
-    
-    }
-    [Serializable]
-    public class Pokemon
-    {
-       public  string name;
-       public  List<SlotType> types;
-        public Sprites sprites;
-
-        [SerializeField]
-        public class SlotType
+        public void MakeApiCall()
         {
-            public int slot;
-            public Type type;
-           
+            IEnumerator apiCall = ApiHelper.Get(url, parameters, OnSuccess, OnFailure);
 
-            [SerializeField]
+            resultField.text = "In Progress";
 
-            public class Type
+            StartCoroutine(apiCall);
+        }
+
+        private void OnFailure(Exception exception)
+        {
+            resultField.text = "Call Error:" + "<br>" + exception.Message;
+        }
+
+        private void OnSuccess(string result)
+        {
+            resultField.text = result;
+        }
+
+        // Pokemon Parsed
+        [Serializable]
+        public class Pokemon
+        {
+            public string name;
+            public List<SlotType> types;
+            public Sprites sprites;
+
+            [Serializable]
+            public class SlotType
             {
-                public string name;
+                public int slot;
+                public Type type;
+
+                [Serializable]
+                public class Type
+                {
+                    public string name;
+                }
+            }
+
+            [Serializable]
+            public class Sprites
+            {
+                public string front_default;
             }
         }
-        [Serializable]
-        public class Sprites
+
+        public void MakePokemonApiCall()
         {
-            public string front_default;
+            IEnumerator apiCall = ApiHelper.Get<Pokemon>(url, parameters, OnPokemonSuccess, OnPokemonFailure);
+
+            resultField.text = "In Progress";
+
+            StartCoroutine(apiCall);
+        }
+
+        private void OnPokemonFailure(Exception exception)
+        {
+            resultField.text = "Call Error:" + "<br>" + exception.Message;
+        }
+
+        private void OnPokemonSuccess(Pokemon result)
+        {
+            resultField.text = "Name: " + result.name;
+            resultField.text += "<br>Types: ";
+
+            foreach (Pokemon.SlotType slotType in result.types)
+            {
+                resultField.text += " " + slotType.type.name;
+            }
+
+            resultField.text += "<br>ImageUrl:<br>" + result.sprites.front_default;
+
+            IEnumerator imageApiCall = ApiHelper.GetTexture(result.sprites.front_default, OnImageSuccess, OnImageFailure);
+            StartCoroutine(imageApiCall);
+        }
+
+        private void OnImageFailure(Exception exception)
+        {
+            imageField.texture = null;
+            imageField.color = Color.red;
+        }
+
+        private void OnImageSuccess(Texture texture)
+        {
+            imageField.color = Color.white;
+            imageField.texture = texture;
         }
     }
-
-    public void MakePokemonApiCall()
-    {
-        IEnumerator apiCall = ApiHelper.Get<Pokemon>(url, parameters, OnPokemonSuccess, OnPokemonFailure);
-
-        resultField.text = "In Progress";
-
-        StartCoroutine(apiCall);
-    }
-
-    private void OnPokemonFailure(Exception exception)
-    {
-        resultField.text = "Call Error:" + "<br>" + exception.Message;
-    }
-
-    private void OnPokemonSuccess(Pokemon result)
-    {
-        resultField.text = "Name: " + result.name;
-        resultField.text += "<br>Types:";
-       
-        foreach(Pokemon.SlotType slotType in result.types) 
-        {
-            resultField.text += " " + slotType.type.name;
-        
-        }
-        resultField.text += "<br> ImageUrl:<br>" + result.sprites.front_default;
-
-        IEnumerator imageApiCall = ApiHelper.GetTexture(result.sprites.front_default,OnImageSuccess,OnImageFailure);
-        StartCoroutine(imageApiCall);
-
-    }
-
-    private void OnImageFailure(Exception exception) 
-    {
-
-        imageField.texture = null;
-        imageField.color = Color.red;
-    
-    
-    }
-
-    private void OnImageSuccess(Texture texture) 
-    {
-        imageField.color = Color.clear;
-        imageField.texture = texture;
-    
-    }
-}
